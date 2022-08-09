459afebcb604a8303ed150e6bd8af670dcda1384
Logan McCalley <mccalleylogan@gmail.com>
2022-08-09 11:07:03 -0400

    getting beers by type

--- src/Components/Beers/index.jsx
+++ src/Components/Beers/index.jsx
@@ -2,17 +2,21 @@ import { useEffect, useState } from "react";
 
 function Beers(){
 const [beerList, setBeerList] = useState();
+const [beerType, setBeerType] = useState('ale')
 useEffect(()=>{
-    fetch(`https://api.sampleapis.com/beers/ale`) //ale,stouts,red-ale
+    fetch(`https://api.sampleapis.com/beers/${beerType}`) //ale,stouts,red-ale
     .then(response => response.json())
     .then(beers => setBeerList(beers))
     .catch(alert);
-},[]);
+},[beerType]);
 if(!beerList){
     return <h2>Loading...</h2>
 }
 return(
     <>
+      <button onClick={() => setBeerType('ale')}>Ale</button>
+      <button onClick={() => setBeerType('stouts')}>Stouts</button>
+      
         <ul>
             {beerList.map(beer => (
                 <li key={beer.id}>{beer.name}</li>
